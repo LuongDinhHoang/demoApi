6e21f72b4cb81af6dd60bf89df281126caa32822
LuongDinhHoang <61779034+LuongDinhHoang@users.noreply.github.com>
2021-04-02 12:31:06 +0700

    gg

---
+++ app/src/main/java/com/example/demoapi/Model.java
@@ -0,0 +1,15 @@
+package com.example.demoapi;
+
+public class Model {
+    String mDescription;
+    String mPost_url;
+    String mTitle;
+    String mWeb_url;
+    public Model(String mDescription, String mPost_url, String mTitle, String mWeb_url) {
+        this.mDescription = mDescription;
+        this.mPost_url = mPost_url;
+        this.mTitle = mTitle;
+        this.mWeb_url = mWeb_url;
+    }
+    
+}
